6ef21fb571f9e6678dd9027919603e7d91dfcf6e
YQS <41525397+TomStone76@users.noreply.github.com>
2019-01-10 13:36:56 -0500

    clean code

--- routes/api/books.js
+++ routes/api/books.js
@@ -2,23 +2,6 @@ const router = require("express").Router();
 const axios = require("axios");
 const booksController = require("../../client/controllers/controller");
 
-// router.route("/books/:search").get(function(req, res) {
-
-    // var searchQuery = req.params.search;
-
-    // axios.get(
-    // "https://www.googleapis.com/books/v1/volumes?q=" + searchQuery + "&key=AIzaSyCeTGHrURWDPgpQKn0ej5EkzWFdN6k2SrQ"
-    
-    // {
-    //     // params: {
-    //     //     q:searchQuery,
-    //     //     key:"AIzaSyCeTGHrURWDPgpQKn0ej5EkzWFdN6k2SrQ"
-    //     // }
-    // })
-//     ).then(({ data: { results } }) => res.json(results))
-//         .catch(err => res.status(422).json(err));
-// })
-
 router.route("/")
     .get(booksController.findAll)
     .post(booksController.create);  
